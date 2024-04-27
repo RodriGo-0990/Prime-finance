72f4321d2af211cbfc463476795e2ec202204bbe
rodrigo.rodrigues <rodrigorodrigues_0990@hotmail.com>
2024-04-27 02:15:35 -0300

    paralax

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './index.css'
 import Home from './components/home.jsx'
 import Section from './components/section.jsx'
+import Paralax from './components/paralax.jsx'
 function App() {
 
   return (
     <>
       <Home/>
+      <Section/>
+      <Paralax/>
       <Section/>
     </>
   )
---
+++ src/components/paralax.jsx
@@ -0,0 +1,15 @@
+import '../css/paralax.css'
+function paralax() {
+    return (
+        <>
+            <div className='wrapper-paralax'>
+                <div className='paralax'>
+                    <h1> Unindo tradição e inovação, nós moldamos seu<p style={{ color: '#20509F' }}> futuro financeiro</p> </h1>
+                </div>
+            </div>
+
+        </>
+    )
+
+}
+export default paralax;
